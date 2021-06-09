78e1445515829e13ebc8567d483e5734b3cb28e9
chriamue <chriamue@users.noreply.github.com>
2021-06-09 06:55:28 +0000

    deploy: lenna-project/lenna-web@7305aee1caa8e48656edd0ce14551646876db600

--- lenna-web.js
+++ lenna-web.js
@@ -4,7 +4,7 @@
 /***/ 225:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-Promise.all(/* import() */[__webpack_require__.e(298), __webpack_require__.e(748), __webpack_require__.e(731)]).then(__webpack_require__.bind(__webpack_require__, 731));
+Promise.all(/* import() */[__webpack_require__.e(298), __webpack_require__.e(748), __webpack_require__.e(97)]).then(__webpack_require__.bind(__webpack_require__, 97));
 
 /***/ })
 
@@ -241,7 +241,7 @@ Promise.all(/* import() */[__webpack_require__.e(298), __webpack_require__.e(748
 /******/ 	
 /******/ 	
 /******/ 		// mini-css-extract-plugin CSS loading
-/******/ 		var cssChunks = {"731":1};
+/******/ 		var cssChunks = {"97":1};
 /******/ 		if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 		else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 			promises.push(installedCssChunks[chunkId] = new Promise(function(resolve, reject) {
@@ -408,7 +408,7 @@ Promise.all(/* import() */[__webpack_require__.e(298), __webpack_require__.e(748
 /******/ 		};
 /******/ 		// no consumes in initial chunks
 /******/ 		var chunkMapping = {
-/******/ 			"731": [
+/******/ 			"97": [
 /******/ 				755
 /******/ 			],
 /******/ 			"748": [

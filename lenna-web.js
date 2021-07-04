5a7e7786d6018e7f90ba731a563ca980971eddf0
chriamue <chriamue@users.noreply.github.com>
2021-07-04 10:15:16 +0000

    deploy: lenna-project/lenna-web@a3a7bd5f52abbcef389bf3f745757ee8b3860e70

--- lenna-web.js
+++ lenna-web.js
@@ -4,7 +4,7 @@
 /***/ 225:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-Promise.all(/* import() */[__webpack_require__.e(595), __webpack_require__.e(748), __webpack_require__.e(871)]).then(__webpack_require__.bind(__webpack_require__, 871));
+Promise.all(/* import() */[__webpack_require__.e(595), __webpack_require__.e(748), __webpack_require__.e(48)]).then(__webpack_require__.bind(__webpack_require__, 48));
 
 /***/ })
 
@@ -367,11 +367,11 @@ Promise.all(/* import() */[__webpack_require__.e(595), __webpack_require__.e(748
 /******/ 		};
 /******/ 		// no consumes in initial chunks
 /******/ 		var chunkMapping = {
+/******/ 			"48": [
+/******/ 				755
+/******/ 			],
 /******/ 			"748": [
 /******/ 				748
-/******/ 			],
-/******/ 			"871": [
-/******/ 				755
 /******/ 			]
 /******/ 		};
 /******/ 		__webpack_require__.f.consumes = (chunkId, promises) => {
@@ -460,7 +460,7 @@ Promise.all(/* import() */[__webpack_require__.e(595), __webpack_require__.e(748
 /******/ 		};
 /******/ 		
 /******/ 		__webpack_require__.f.miniCss = (chunkId, promises) => {
-/******/ 			var cssChunks = {"871":1};
+/******/ 			var cssChunks = {"48":1};
 /******/ 			if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 			else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 				promises.push(installedCssChunks[chunkId] = loadStylesheet(chunkId).then(() => {
